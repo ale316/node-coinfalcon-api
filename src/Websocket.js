3d029ff4fd540f956b53ac8345b2e55a7e972780
Alessandro Marin <alessandro.m316@gmail.com>
2018-01-03 16:29:16 +0800

    adds a subscription function to websocket object

--- src/Websocket.js
+++ src/Websocket.js
@@ -1,20 +1,65 @@
 const Cable = require("es6-actioncable").default;
-const w3cwebsocket = require('websocket').w3cwebsocket;
+const w3cwebsocket = require("websocket").w3cwebsocket;
+const EventEmitter = require("events");
 
-class Websocket {
+class Websocket extends EventEmitter {
+    constructor() {
+        super();
+
+        this.WS_URL = "wss://ws.coinfalcon.com";
+        this.WS_ORIGIN = "https://coinfalcon.com";
+        this.consumer = this._initConsumer(this.WS_URL, this.WS_ORIGIN);
+
+        // methods binding
+        this.subscribe = this.subscribe.bind(this);
+    }
+
+    /* Private Methods */
+
     /**
-     * @param {Object} opts
-     * @param {String} opts.key
-     * @param {String} opts.secret
+     * @param {String} url
+     * @param {String} origin
      */
-    constructor(opts) {
-        this.key = opts.key;
-        this.secret = opts.secret;
+    _initConsumer(url, origin) {
+        return Cable.createConsumer(url, {
+            createWebsocket: options => {
+                return new w3cwebsocket(
+                    url,
+                    options.protocols,
+                    origin,
+                    options.headers,
+                    options.extraRequestOptions
+                );
+            }
+        });
     }
 
     /**
-     * @param {}
+     * @param {String} channel
+     * @param {Object} payload [optional]
      */
+    _normalize(channel, payload = {}) {
+        return { evt: "test", data: payload };
+    }
+
+    /* Public Methods */
+
+    /**
+     * @param {String} channel
+     * @param {Object} opts [optional]
+     */
+    subscribe(channel, opts = {}) {
+        const options = Object.assign({}, { channel: channel }, opts);
+        this.consumer.subscriptions.create(options, {
+            connected: () => {
+                this.emit(`${channel}:connected`);
+            },
+            received: payload => {
+                const { evt, data } = this._normalize(channel, payload);
+                this.emit(`${channel}:${evt}`, data);
+            }
+        });
+    }
 }
 
 module.exports = Websocket;
